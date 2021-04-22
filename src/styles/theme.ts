6d6de472bd5badf72e2d9c86baf00de4be2819c9
Anderson Rodrigues <anddersonrds@gmail.com>
2021-04-21 23:25:08 -0300

    Add ThemeStyle

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -7,32 +7,33 @@ export default {
     radius: '0.4rem'
   },
   font: {
-    family:
-      "Poppins, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif",
+    family: 'Poppins, sans-serif',
     light: 300,
     normal: 400,
     bold: 600,
     sizes: {
-      xsmall: '1.2rem',
-      small: '1.4rem',
-      medium: '1.6rem',
-      large: '1.8rem',
+      xxsmall: '0.5rem',
+      xsmall: '1rem',
+      small: '1.25rem',
+      medium: '1.5rem',
+      large: '1.75rem',
       xlarge: '2.0rem',
-      xxlarge: '2.8rem',
+      xxlarge: '2.5rem',
       huge: '5.2rem'
     }
   },
   colors: {
-    primary: '#F231A5',
-    secondary: '#3CD3C1',
-    mainBg: '#06092B',
-    lightBg: '#F2F2F2',
-    white: '#FAFAFA',
-    black: '#030517',
-    lightGray: '#EAEAEA',
-    gray: '#8F8F8F',
-    darkGray: '#2E2F42',
-    red: '#FF6347'
+    white: '#FFF',
+    gray50: '#F7F8FA',
+    gray100: '#E6E8EB',
+    gray200: '#AFB2B1',
+    gray500: '#808080',
+    gray800: '#494D4B',
+    green500: '#04D361',
+    purple300: '#9F75FF',
+    purple400: '#9164FA',
+    purple500: '#8257E5',
+    purple800: '#6F48C9'
   },
   spacings: {
     xxsmall: '0.8rem',
