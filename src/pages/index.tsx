a8768fe3ec2d90698ceabc9614d9421274c3ffdc
Anderson Rodrigues <anddersonrds@gmail.com>
2021-04-25 23:46:53 -0300

    Add Home Page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,51 @@
-export default function Home() {
-  return <h2>Espero que goste de usar esse boilerplate :)</h2>
+import { GetStaticProps } from 'next'
+import { format, parseISO } from 'date-fns'
+import ptBR from 'date-fns/locale/pt-BR'
+import { api } from 'services/api'
+
+import { convertDurationToTimeString } from 'utils/convertDurationToTimeString'
+
+import Home, { HomeTemplateProps } from 'templates/Home'
+
+export default function Index(props: HomeTemplateProps) {
+  return <Home {...props} />
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const { data } = await api.get('episodes', {
+    params: {
+      _limite: 12,
+      _sort: 'published_at',
+      _order: 'desc'
+    }
+  })
+
+  const episodes = data.map((episode) => {
+    return {
+      id: episode.id,
+      title: episode.title,
+      description: episode.description,
+      thumbnail: episode.thumbnail,
+      members: episode.members,
+      publishedAt: format(parseISO(episode.published_at), 'd MMM yy', {
+        locale: ptBR
+      }),
+      duration: Number(episode.file.duration),
+      durationAsString: convertDurationToTimeString(
+        Number(episode.file.duration)
+      ),
+      url: episode.file.url
+    }
+  })
+
+  const lastestEpisodes = episodes.slice(0, 2)
+  const allEpisodes = episodes.slice(2, episodes.length)
+
+  return {
+    props: {
+      lastestEpisodes,
+      allEpisodes
+    },
+    revalidate: 60 * 60 * 8
+  }
 }
